01fb4b0b55df873594f76abb63ac09aaad57228d
MatiasHolmNielsen <10519141@ucn.dk>
2025-04-24 09:36:42 +0200

    Auction & auctionStatus

--- Code/AuctionHouse/AuctionHouse.ClassLibrary/Auction.cs
+++ Code/AuctionHouse/AuctionHouse.ClassLibrary/Auction.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using AuctionHouse.ClassLibrary.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AuctionHouse.ClassLibrary
 {
-    internal class Auction
+    public class Auction
     {
+
+        public Auction(string auctionID, DateTime startTime, DateTime endTime, decimal startPrice, decimal buyOutPrice, decimal minimumBidIncrement)
+        {
+            this.AuctionID = auctionID;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.StartPrice = startPrice;
+            this.BuyOutPrice = buyOutPrice;
+            this.MinimumBidIncrement = minimumBidIncrement;
+            this.AuctionStatus = AuctionStatus;
+            this.Version = 1;
+        }
+
+        public string AuctionID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal StartPrice { get; set; }
+        public decimal BuyOutPrice { get; set; }
+        public decimal MinimumBidIncrement { get; set; }
+        public AuctionStatus AuctionStatus { get; set; }
+        public int Version { get; set; }
+        public String Notify { get; set; } 
+
     }
 }
