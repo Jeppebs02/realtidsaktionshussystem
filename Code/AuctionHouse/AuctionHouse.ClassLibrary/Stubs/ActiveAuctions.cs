c55b6c00154ab48b49814faa72df0922467e7ff6
Oscar Hermann <oscar.hermann94@gmail.com>
2025-04-25 13:14:01 +0200

    part2

---
+++ Code/AuctionHouse/AuctionHouse.ClassLibrary/Stubs/ActiveAuctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionHouse.ClassLibrary.Model;
+
+namespace AuctionHouse.ClassLibrary.Stubs
+{
+    public class ActiveAuctions
+    {
+        public List<Auction> TestList { get; set; }
+    }
+}
--- Code/AuctionHouse/AuctionHouse.WebSite/Pages/Auction/Active/Index.cshtml.cs
+++ Code/AuctionHouse/AuctionHouse.WebSite/Pages/Auction/Active/Index.cshtml.cs
@@ -1,18 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using AuctionHouse.ClassLibrary.Model;
+using AuctionHouse.ClassLibrary;
 using AuctionHouse.WebAPI;
 
 namespace AuctionHouse.WebSite.Pages.Auction.Active
 {
     public class IndexModel : PageModel
     {
-        public List<AuctionHouse.ClassLibrary.Model.Auction> ActiveAuctions { get; set; }
+        public List<AuctionHouse.ClassLibrary.Model.Auction> ActiveAuctions { get; set; } 
 
         public void OnGet()
         {
             // Load active auctions from DB or service
-            ActiveAuctions = AuctionHouse.WebAPI.GetActiveAuctions(); // just an example
+            //ActiveAuctions = AuctionHouse.WebAPI.GetActiveAuctions(); // just an example
+            new List<AuctionHouse.ClassLibrary.Model.Auction> ActiveAuctions(AuctionHouse.ClassLibrary.Stubs.ActiveAuctions.get(TestList));
+        }
+
+        public List<AuctionHouse.ClassLibrary.Model.Auction> GetActiveAuctions()
+        {
+            //TODO - make logic - deserialse json from WebAPI
+            return null;
         }
     }
+}
 
