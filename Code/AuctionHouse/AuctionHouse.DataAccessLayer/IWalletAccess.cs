8743d1956eb778a8ffe369acd25753322df68b97
MatiasHolmNielsen <10519141@ucn.dk>
2025-04-25 10:14:03 +0200

    WEB & Dataacces

---
+++ Code/AuctionHouse/AuctionHouse.DataAccessLayer/IWalletAccess.cs
@@ -0,0 +1,15 @@
+﻿using AuctionHouse.ClassLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace AuctionHouse.DataAccessLayer
+{
+    public interface IWalletAccess
+    {
+        Wallet GetWalletForUser(string username);
+    }
+}
