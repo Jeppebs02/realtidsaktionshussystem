56d6cc453b4643723004861188a52abfe9559f3b
Oscar Hermann <oscar.hermann94@gmail.com>
2025-04-28 10:21:59 +0200

    searchBar

--- Code/AuctionHouse/AuctionHouse.WebSite/Pages/Auction/Active/Index.cshtml.cs
+++ Code/AuctionHouse/AuctionHouse.WebSite/Pages/Auction/Active/Index.cshtml.cs
@@ -3,17 +3,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using AuctionHouse.ClassLibrary.Stubs;
 using AuctionHouse.WebAPI;
 using System.Collections.Generic;
+using AuctionHouse.ClassLibrary.Enum;
 
 namespace AuctionHouse.WebSite.Pages.Auction.Active
 {
     public class IndexModel : PageModel
     {
         public List<AuctionHouse.ClassLibrary.Model.Auction> TestAuctions { get; set; }
+        public List<AuctionHouse.ClassLibrary.Model.Auction> FilteredAuctions { get; set; } = new();
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchTerm { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public Category? SelectedCategory { get; set; }
 
         public void OnGet()
         {
             // Load test data
             TestAuctions = AuctionHouse.ClassLibrary.Stubs.AuctionTestData.GetTestAuctions();
+
+            // Start with all auctions
+            FilteredAuctions = TestAuctions;
+
+            // Apply search term
+            if (!string.IsNullOrWhiteSpace(SearchTerm))
+            {
+                FilteredAuctions = FilteredAuctions
+                    .Where(a => a.item.Name.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+
+            // Apply category filter
+            if (SelectedCategory.HasValue)
+            {
+                FilteredAuctions = FilteredAuctions
+                    .Where(a => a.item.Category == SelectedCategory)
+                    .ToList();
+            }
         }
 
         public List<AuctionHouse.ClassLibrary.Model.Auction> GetActiveAuctions()
