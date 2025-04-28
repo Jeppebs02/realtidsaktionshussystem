bcff23ef55f66c826ef8806b04822060f23b984a
Oscar Hermann <oscar.hermann94@gmail.com>
2025-04-28 09:19:21 +0200

    OpretSideMedAktiveAuktioner

--- Code/AuctionHouse/AuctionHouse.WebSite/Pages/Auction/Active/Index.cshtml.cs
+++ Code/AuctionHouse/AuctionHouse.WebSite/Pages/Auction/Active/Index.cshtml.cs
@@ -1,26 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using AuctionHouse.ClassLibrary;
+using AuctionHouse.ClassLibrary.Stubs;
 using AuctionHouse.WebAPI;
+using System.Collections.Generic;
 
 namespace AuctionHouse.WebSite.Pages.Auction.Active
 {
     public class IndexModel : PageModel
     {
-        public List<AuctionHouse.ClassLibrary.Model.Auction> ActiveAuctions { get; set; } 
+        public List<AuctionHouse.ClassLibrary.Model.Auction> TestAuctions { get; set; }
 
         public void OnGet()
         {
-            // Load active auctions from DB or service
-            //ActiveAuctions = AuctionHouse.WebAPI.GetActiveAuctions(); // just an example
-            new List<AuctionHouse.ClassLibrary.Model.Auction> ActiveAuctions(AuctionHouse.ClassLibrary.Stubs.ActiveAuctions.get(TestList));
+            // Load test data
+            TestAuctions = AuctionHouse.ClassLibrary.Stubs.AuctionTestData.GetTestAuctions();
         }
 
         public List<AuctionHouse.ClassLibrary.Model.Auction> GetActiveAuctions()
         {
-            //TODO - make logic - deserialse json from WebAPI
+            // TODO - make logic to fetch data from WebAPI
             return null;
         }
     }
 }
+
 
