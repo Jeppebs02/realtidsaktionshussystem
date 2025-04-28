97774e30014e328f132c855f20f16c54e22331b0
greenpure61 <10519150@ucn.dk>
2025-04-28 13:15:17 +0200

    CreateUser page

---
+++ Code/AuctionHouse/AuctionHouse.WebSite/Pages/CreateUser/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using AuctionHouse.ClassLibrary.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace AuctionHouse.WebSite.Pages.CreateUser
+{
+    public class IndexModel : PageModel
+    {
+
+        //Error message to show on the page.
+        [BindProperty]
+        public String? errorMessage { get; set; } = null;
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostCreateUserAsync(string userName, string password, string firstName, string lastName, string email, string phoneNumber, string address)
+        {
+            User newUser = new User(userName, password, firstName, lastName, email, phoneNumber, address, new Wallet(0, 0));
+
+            var JSONData = JsonConvert.SerializeObject(newUser);
+
+            return Page();
+        }
+
+    }
+}
