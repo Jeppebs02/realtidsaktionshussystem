1dfff8831f81c95a1155230a2931fa93e33bd18b
MatiasHolmNielsen <10519141@ucn.dk>
2025-05-01 13:41:30 +0200

    Create IWalletDaoTest.cs

---
+++ Code/AuctionHouse/AuctionHouse.Test/DaoTests/IWalletDaoTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouse.Test.DaoTests
+{
+    public class IWalletDaoTest
+    {
+
+        [Fact]
+        public void GetByUserIdTest()
+        {
+
+
+        }
+
+
+    }
+}
