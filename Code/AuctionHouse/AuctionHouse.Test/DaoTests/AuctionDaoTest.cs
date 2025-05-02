7fdb48bc392cccc3ee101c98e0f72e9b2068840c
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-05-02 11:01:58 +0200

    Update AuctionDaoTest.cs

--- Code/AuctionHouse/AuctionHouse.Test/DaoTests/AuctionDaoTest.cs
+++ Code/AuctionHouse/AuctionHouse.Test/DaoTests/AuctionDaoTest.cs
@@ -1,4 +1,5 @@
 ﻿using AuctionHouse.DataAccessLayer.Interfaces;
+using AuctionHouse.ClassLibrary.Enum;
 using AuctionHouse.ClassLibrary.Model;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,7 @@ namespace AuctionHouse.Test.DaoTests
 
         private readonly IAuctionDao _auctionDao;
 
-        [Fact]
+        [Fact(Skip = "Not needed rn")]
         public async Task GetWithinDateRangeAsync_ShouldReturnAuctions_WhenDateRangeIsValid()
         {
             // Arrange  
@@ -38,21 +39,24 @@ namespace AuctionHouse.Test.DaoTests
             // Act  
             IEnumerable<Auction> auctions = await _auctionDao.GetAllByUserIDAsync(userId);
 
-            IEnumerable<Auction> auctions2 = await _auctionDao.GetAllAsync<Auction>();
+            IEnumerable<Auction> allAuctions = await _auctionDao.GetAllAsync<Auction>();
+
+            IEnumerable<Auction> auctions2 = new List<Auction>();
             // Assert  
             Assert.NotNull(auctions);
 
-            foreach (var auction in auctions2)
+            foreach (var auction in allAuctions)
             {
-                if (auction.item.User.userId== userId)
+                if (auction.item.User.userId == userId)
                 {
-
+                    auctions2.Append(auction);
                 }
             }
 
             //Get all auctions that the user has an item in.
+            Assert.Equal(auctions, auctions2);
 
-
+            //will Possibly break
         }
 
 
@@ -64,12 +68,100 @@ namespace AuctionHouse.Test.DaoTests
             // Act  
             IEnumerable<Auction> auctions = await _auctionDao.GetAllByBidsAsync(userId);
 
-            IEnumerable<Auction> auctions2 = await _auctionDao.GetAllByUserIDAsync(userId);
+            IEnumerable<Auction> auctions2 = new List<Auction>();
+
+            IEnumerable<Auction> allAuctions = await _auctionDao.GetAllAsync<Auction>();
+
+            foreach (var auction in allAuctions)
+            {
+                if (auction.Bids.Any(b => b.User.userId == userId))
+                {
+                    auctions2.Append(auction);
+                }
+            }
+
             // Assert  
             Assert.NotNull(auctions);
-            //Does Auction have a userid?
+            Assert.Equal(auctions, auctions2);
+            
         }
 
+
+        [Fact]
+        public async Task GetAllActiveAsync_ShouldReturnActiveAuctions()
+        {
+            // Arrange  
+            // Act  
+            IEnumerable<Auction> auctions = await _auctionDao.GetAllActiveAsync();
+            // Assert  
+            Assert.NotNull(auctions);
+            // Check that all auctions are active
+            Assert.All(auctions, auction => Assert.Equal(AuctionStatus.ACTIVE, auction.AuctionStatus));
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnAllAuctions()
+        {
+            // Arrange  
+            // Act  
+            IEnumerable<Auction> auctions = await _auctionDao.GetAllAsync<Auction>();
+            // Assert  
+            Assert.NotNull(auctions);
+            // Check that all auctions are returned
+            Assert.NotEmpty(auctions);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnAuction_WhenIdExists()
+        {
+            // Arrange  
+            int auctionId = 1;
+            // Act  
+            Auction auction = await _auctionDao.GetByIdAsync<Auction>(auctionId);
+            // Assert  
+            Assert.NotNull(auction);
+            Assert.Equal(auctionId, auction.AuctionID);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldAddAuction()
+        {
+            // Arrange  
+            Wallet wallet = new Wallet(1000, 0,0);
+            User user = new User("testUser", "testPassword", "firstname", "lastname", "email", "phonenr", "address", wallet);
+            user.userId = 1;
+            Item item = new Item(user, "testItem", "testDescription", Category.ELECTRONICS, new byte[0], ItemStatus.AVAILABLE);
+            Auction auction = new Auction(DateTime.Now, DateTime.Now.AddDays(1), 100, 200, 10, false, item);
+            // Act  
+            int newAuctionId = await _auctionDao.InsertAsync(auction);
+            // Assert  
+            Assert.True(newAuctionId>0);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_ShouldUpdateAuction()
+        {
+            // Arrange  
+            int auctionId = 1;
+            Auction auction = await _auctionDao.GetByIdAsync<Auction>(auctionId);
+            auction.StartPrice = 150;
+            // Act  
+            bool result = await _auctionDao.UpdateAsync(auction);
+            // Assert  
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ShouldDeleteAuction()
+        {
+            // Arrange  
+            int auctionId = 1;
+            Auction auction = await _auctionDao.GetByIdAsync<Auction>(auctionId);
+            // Act  
+            bool result = await _auctionDao.DeleteAsync(auction);
+            // Assert  
+            Assert.True(result);
+        }
 
 
     }
