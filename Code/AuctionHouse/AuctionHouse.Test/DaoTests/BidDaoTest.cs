f03413da7e5653e16b5815cded7494c6c0a00dbd
greenpure61 <10519150@ucn.dk>
2025-05-02 10:34:06 +0200

    BIDDAOTEST

--- Code/AuctionHouse/AuctionHouse.Test/DaoTests/BidDaoTest.cs
+++ Code/AuctionHouse/AuctionHouse.Test/DaoTests/BidDaoTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AuctionHouse.ClassLibrary.Model;
+using AuctionHouse.DataAccessLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,56 @@ using System.Threading.Tasks;
 
 namespace AuctionHouse.Test.DaoTests
 {
-    internal class BidDaoTest
+    public class BidDaoTest
     {
+        private readonly IBidDao _bidDao;
+
+        
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnListOfBids()
+        {
+            // Act
+            List<Bid> bids = await _bidDao.GetAllAsync<Bid>();
+                      
+            // Assert   
+            Assert.NotEmpty(bids);
+        }
+
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnBid_WhenBidExists()
+        {
+            // Arrange  
+            int bidId = 1;
+            // Act  
+            Bid? bid = await _bidDao.GetByIdAsync<Bid>(bidId);
+            // Assert   
+            Assert.NotNull(bid);
+            Assert.Equal(bidId, bid?.BidId);
+        }
+
+        [Fact]
+        public async Task GetLatestByAuctionId_ShouldReturnLatestBid_WhenBidsExist()
+        {
+            // Arrange  
+            int auctionId = 1;
+            // Act  
+            Bid? bid = await _bidDao.GetLatestByAuctionId(auctionId);
+            // Assert   
+            Assert.NotNull(bid);
+            Assert.Equal(600, bid.Amount);
+        }
+
+        [Fact]
+        public async Task InsertAsync_ShouldReturnId_WhenBidIsInserted()
+        {
+            // Arrange  
+            Bid bid = new Bid(1, 500, DateTime.Now, 1);
+            // Act  
+            int id = await _bidDao.InsertAsync(bid);
+            // Assert  
+            Assert.True(id > 5);
+        }
     }
 }
