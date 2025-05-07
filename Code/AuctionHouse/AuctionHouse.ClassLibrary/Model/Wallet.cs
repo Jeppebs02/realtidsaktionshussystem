083639b1ee6456fe30faa40a6a580121b605626f
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-05-07 11:20:55 +0200

    Update Wallet.cs

--- Code/AuctionHouse/AuctionHouse.ClassLibrary/Model/Wallet.cs
+++ Code/AuctionHouse/AuctionHouse.ClassLibrary/Model/Wallet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AuctionHouse.ClassLibrary.Model
@@ -32,7 +33,7 @@ namespace AuctionHouse.ClassLibrary.Model
         public byte[] Version { get; set; }
 
         public List<Transaction> Transactions { get; set; }
-
+        [JsonIgnore]
         public int? UserId { get; set; }
         #endregion
 
