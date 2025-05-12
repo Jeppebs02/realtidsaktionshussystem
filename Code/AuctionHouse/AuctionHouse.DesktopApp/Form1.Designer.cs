3a2a473acd4c8f0d9856b218d26017ef51cf961f
greenpure61 <10519150@ucn.dk>
2025-05-12 09:31:18 +0200

    WinForms Visuelt look only

--- Code/AuctionHouse/AuctionHouse.DesktopApp/Form1.Designer.cs
+++ Code/AuctionHouse/AuctionHouse.DesktopApp/Form1.Designer.cs
@@ -28,12 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button4 = new Button();
+            Emailtxtbox = new TextBox();
+            Addresstxtbox = new TextBox();
+            Passwordtxtbox = new TextBox();
+            Usernametxtbox = new TextBox();
+            LastNametxtbox = new TextBox();
+            FirstNametxtbox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            PhoneNumbertxtbox = new TextBox();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(65, 189);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(108, 29);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Cant Buy";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(65, 224);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(106, 29);
+            checkBox2.TabIndex = 2;
+            checkBox2.Text = "Cant Sell";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(349, 319);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(786, 304);
+            listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(65, 319);
+            button2.Name = "button2";
+            button2.Size = new Size(267, 64);
+            button2.TabIndex = 4;
+            button2.Text = "Get All Users";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(65, 559);
+            button3.Name = "button3";
+            button3.Size = new Size(267, 64);
+            button3.TabIndex = 5;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(65, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(65, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Search User";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(285, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 31);
+            button1.TabIndex = 8;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(65, 489);
+            button4.Name = "button4";
+            button4.Size = new Size(267, 64);
+            button4.TabIndex = 9;
+            button4.Text = "Delete User";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Emailtxtbox
+            // 
+            Emailtxtbox.Location = new Point(349, 241);
+            Emailtxtbox.Name = "Emailtxtbox";
+            Emailtxtbox.Size = new Size(200, 31);
+            Emailtxtbox.TabIndex = 10;
+            // 
+            // Addresstxtbox
+            // 
+            Addresstxtbox.Location = new Point(649, 241);
+            Addresstxtbox.Name = "Addresstxtbox";
+            Addresstxtbox.Size = new Size(200, 31);
+            Addresstxtbox.TabIndex = 11;
+            // 
+            // Passwordtxtbox
+            // 
+            Passwordtxtbox.Location = new Point(935, 241);
+            Passwordtxtbox.Name = "Passwordtxtbox";
+            Passwordtxtbox.Size = new Size(200, 31);
+            Passwordtxtbox.TabIndex = 12;
+            // 
+            // Usernametxtbox
+            // 
+            Usernametxtbox.Location = new Point(935, 152);
+            Usernametxtbox.Name = "Usernametxtbox";
+            Usernametxtbox.Size = new Size(200, 31);
+            Usernametxtbox.TabIndex = 15;
+            // 
+            // LastNametxtbox
+            // 
+            LastNametxtbox.Location = new Point(649, 152);
+            LastNametxtbox.Name = "LastNametxtbox";
+            LastNametxtbox.Size = new Size(200, 31);
+            LastNametxtbox.TabIndex = 14;
+            // 
+            // FirstNametxtbox
+            // 
+            FirstNametxtbox.Location = new Point(649, 69);
+            FirstNametxtbox.Name = "FirstNametxtbox";
+            FirstNametxtbox.Size = new Size(200, 31);
+            FirstNametxtbox.TabIndex = 13;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(349, 213);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 16;
+            label2.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(649, 213);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 25);
+            label3.TabIndex = 17;
+            label3.Text = "Address";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(935, 213);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 25);
+            label4.TabIndex = 18;
+            label4.Text = "Password";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(935, 124);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 25);
+            label5.TabIndex = 21;
+            label5.Text = "Username";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(649, 124);
+            label6.Name = "label6";
+            label6.Size = new Size(95, 25);
+            label6.TabIndex = 20;
+            label6.Text = "Last Name";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(649, 41);
+            label7.Name = "label7";
+            label7.Size = new Size(97, 25);
+            label7.TabIndex = 19;
+            label7.Text = "First Name";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(349, 124);
+            label8.Name = "label8";
+            label8.Size = new Size(132, 25);
+            label8.TabIndex = 23;
+            label8.Text = "Phone Number";
+            // 
+            // PhoneNumbertxtbox
+            // 
+            PhoneNumbertxtbox.Location = new Point(349, 152);
+            PhoneNumbertxtbox.Name = "PhoneNumbertxtbox";
+            PhoneNumbertxtbox.Size = new Size(200, 31);
+            PhoneNumbertxtbox.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1185, 683);
+            Controls.Add(label8);
+            Controls.Add(PhoneNumbertxtbox);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(Usernametxtbox);
+            Controls.Add(LastNametxtbox);
+            Controls.Add(FirstNametxtbox);
+            Controls.Add(Passwordtxtbox);
+            Controls.Add(Addresstxtbox);
+            Controls.Add(Emailtxtbox);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private ListBox listBox1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button4;
+        private TextBox Emailtxtbox;
+        private TextBox Addresstxtbox;
+        private TextBox Passwordtxtbox;
+        private TextBox Usernametxtbox;
+        private TextBox LastNametxtbox;
+        private TextBox FirstNametxtbox;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox PhoneNumbertxtbox;
     }
 }
